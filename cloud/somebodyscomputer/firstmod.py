fe0ffe1765504312079f5ff1798987c691bdd5ff
Mark S. Maglana <mmaglana@gmail.com>
2016-09-23 14:16:30 -0700

    Others

--- cloud/somebodyscomputer/firstmod.py
+++ cloud/somebodyscomputer/firstmod.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 # Make coding more python3-ish
 from __future__ import (absolute_import, division)
 __metaclass__ = type
@@ -7,6 +21,46 @@ from urllib2 import URLError
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.urls import open_url
+
+
+DOCUMENTATION = '''
+---
+module: firsmod
+short_description: Downloads stuff from the interwebs
+description:
+    - Downloads stuff
+    - Saves said stuff
+version_added: "2.2"
+options:
+  url:
+    description:
+      - The location of the stuff to download
+    required: false
+    default: null
+  dest:
+    description:
+      - Where to save the stuff
+    required: false
+    default: /tmp/firstmod
+
+author:
+    - "Your Name Here (@yourgithubusernamehere)"
+'''
+
+RETURN = '''
+msg:
+    description: Just returns a friendly message
+    returned: always
+    type: string
+    sample: Hi there!
+'''
+
+EXAMPLES = '''
+# Download then save to your home dir
+- firstmod:
+    url: https://www.relaxdiego.com
+    dest: ~/relaxdiego.com.txt
+'''
 
 
 class FetchError(Exception):
